21a097424bb24d5b31217b3f36b6f2af39229e17
Andrew Danilovic <a.danilovic@ucla.edu>
2019-05-21 19:26:57 -0700

    Add two grep version test cases

--- grep/grep_test.py
+++ grep/grep_test.py
@@ -1,11 +1,34 @@
 #!/usr/bin/python3
 
+"""
+Standard python import statements
+"""
 import unittest
+import subprocess
+import re
 
 class GrepTest(unittest.TestCase):
+    """
+    Tests of the grep linux command line utility.
+    """
 
-    def test_grep(self):
-        self.assertEqual(True, True)
+    def test_grep_version(self):
+        """
+        Verify that the grep version output contains a version number with the expected format.
+        """
+        grep_version_output = subprocess.check_output(['grep', '--v'])
+        self.assertEqual(type(grep_version_output), bytes)
+        grep_version_output_string =  grep_version_output.decode()
+        result = re.match('grep \(GNU grep\) [0-9].[0-9]', grep_version_output_string)
+        self.assertNotEqual(result, None)
+
+    def test_grep_version_short_long_are_equal(self):
+        """
+        Verify that the --v and --version arguments produce the same output.
+        """
+        grep_short_version_output = subprocess.check_output(['grep', '--v'])
+        grep_long_version_output  = subprocess.check_output(['grep', '--version'])
+        self.assertEqual(grep_short_version_output, grep_long_version_output)
 
 if __name__ == '__main__':
     unittest.main()
